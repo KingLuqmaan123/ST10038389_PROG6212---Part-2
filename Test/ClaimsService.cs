133029f9fe2c659aca5972dc84a5cd2c1153f068
Luqmaan Aziz <114137264+KingLuqmaan123@users.noreply.github.com>
2024-10-17 13:48:59 +0200

    Update ClaimsService.cs

--- Test/ClaimsService.cs
+++ Test/ClaimsService.cs
@@ -9,3 +9,6 @@ namespace YourProjectName.Tests
         }
     }
 }
+//Unit Testing for ClaimsService Class:
+//Testing to Calculate the Total Amount of Hours worked and hourly rate in decimal form.
+//Added an exception contructor  to inform the user that incase the data doesn't pass through or did not calculate correctly it will output an error message, "PLEASE TRY AGAIN"!
